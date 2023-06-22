c48875b5895177b2f5a2590290737e74ce3b3896
chavda-mitul <20dce020@charusat.edu.in>
2023-06-22 22:03:25 +0530

    added the color changing feature

--- client/src/Components/Draw.tsx
+++ client/src/Components/Draw.tsx
@@ -2,23 +2,26 @@ import React, { useState, useRef, useEffect } from "react";
 import { Stage, Layer, Line, Text } from "react-konva";
 import { io } from "socket.io-client";
 
+type LineData = {
+  tool: string;
+  points: number[];
+  color: string;
+};
+
 const Draw = () => {
   const [tool, setTool] = useState<string>("pen");
-  const [lines, setLines] = useState<any[]>([]);
-  const [lines_, setLines_] = useState<any[]>([]);
+  const [lines, setLines] = useState<LineData[]>([]);
   const stageRef = useRef<any>(null);
   const isDrawing = useRef<boolean>(false);
   const socket = useRef<any>(null);
+  const [color, setColor] = useState("black");
 
   const handleMouseDown = () => {
     isDrawing.current = true;
     const stage = stageRef.current;
     const pos = stage.getPointerPosition();
-    const newLine = { tool, points: [pos.x, pos.y] };
+    const newLine = { tool, points: [pos.x, pos.y], color };
     setLines((prevLines) => [...prevLines, newLine]);
-    // socket.current.emit("draw", newLine);
-    // console.log("down", lines);
-    // console.log(newLine);
   };
 
   const handleMouseMove = () => {
@@ -37,12 +40,10 @@ const Draw = () => {
     const updatedLines = [...lines];
     updatedLines.splice(lines.length - 1, 1, updatedLastLine);
     setLines(updatedLines);
-    // console.log("updated", lines);
   };
 
   const handleMouseUp = () => {
     isDrawing.current = false;
-    console.log("lines up", lines);
     socket.current.emit("drawing", lines);
   };
 
@@ -53,15 +54,34 @@ const Draw = () => {
       socket.current.disconnect();
     };
   }, []);
+
   useEffect(() => {
-    socket.current.on("drawing", (syncLines: any[]) => {
+    socket.current.on("drawing", (syncLines: LineData[]) => {
       setLines(syncLines);
     });
-    console.log("line updated ", lines);
-  }, [lines]);
+  }, []);
 
   return (
     <div>
+      <select
+        value={tool}
+        onChange={(e) => {
+          setTool(e.target.value);
+        }}
+      >
+        <option value="pen">Pen</option>
+        <option value="eraser">Eraser</option>
+      </select>
+      <select
+        value={color}
+        onChange={(e) => {
+          setColor(e.target.value);
+        }}
+      >
+        <option value="black">Black</option>
+        <option value="red">Red</option>
+        <option value="green">Green</option>
+      </select>
       <Stage
         width={window.innerWidth}
         height={window.innerHeight}
@@ -76,8 +96,8 @@ const Draw = () => {
             <Line
               key={i}
               points={line.points}
-              stroke="#df4b26"
-              strokeWidth={5}
+              stroke={line.color}
+              strokeWidth={4}
               tension={0.5}
               lineCap="round"
               lineJoin="round"
@@ -88,15 +108,6 @@ const Draw = () => {
           ))}
         </Layer>
       </Stage>
-      <select
-        value={tool}
-        onChange={(e) => {
-          setTool(e.target.value);
-        }}
-      >
-        <option value="pen">Pen</option>
-        <option value="eraser">Eraser</option>
-      </select>
     </div>
   );
 };
